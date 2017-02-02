f7107def59adb0b8b384cda535cbb43907d61df3
AymanxD <aymanmohatarem@gmail.com>
2017-02-02 09:53:44 -0500

    Comments

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck"
 
       var twitch = function() {
         $.getJSON("https://api.twitch.tv/kraken/streams/" + channels[i] + "?client_id=vkt06djc7pfa24zsgs0dht9lfd5p3x", function(datas){
+
+          // Check if data is null or not = Online or Offline/deleted
           if (datas.stream === null) {
              offline(datas);
           }
@@ -13,12 +15,14 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck"
             }
           })
 
+        // Creates Online info box on users
         var online =  function(datas) {
           $("#Online").append("<a class='links' target='_blank' href="+
             datas.stream.channel.url + "><div class='on row'><img class='img col-1-3' src=" + datas.stream.channel.logo + ">" +
             "<h4 class='name col-1-3'>" + datas.stream.channel.name + "</h4></br></br><p class='status'>" + datas.stream.channel.status + "</p></div></a>");
           }
 
+      //Creates Offline info box on users or if the user does not exist. Used ajax instead of getJSON to get jsonp
         var offline = function(datas) {
           $.ajax({
                 url:datas._links.channel  + "?client_id=vkt06djc7pfa24zsgs0dht9lfd5p3x",
@@ -26,7 +30,7 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck"
                 type:"GET",
                 success:function(datac){
           if (datac.display_name === undefined){
-            $("#Deleted").append("<div class='deleted'><h4>" + datac.message + ".</h4></div>");
+            $("#Deleted").append("<div class='deleted'><p>" + datac.message + ".</p></div>");
           }
           else {
             $("#Offline").append("<a class='links' target='_blank' href="+
@@ -37,10 +41,13 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck"
         })
       }
 
+      // loops through the channels of choice
       }
     for(i=0; i< channels.length; i++) {
       twitch();
     }
+
+    // Actions which determine what each button does
 
     $(".All").click(function() {
       $("#Online").show(500);
